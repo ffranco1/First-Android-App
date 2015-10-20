45afae19ab71ce5039789eadd2060b4b16f9e119
Franco <ffranco1@go.pasadena.edu>
2015-10-20 06:09:34 -0700

    succesfully figured out how to send an sms

--- app/src/main/java/com/example/franciscofranco/playingaround/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/playingaround/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -67,9 +69,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        String celebrity = edit_text.getText().toString();
+        String sms = edit_text.getText().toString();
         edit_text.setText("");
-        // comment 
+        String phoneNo = "8186603057";
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+            Toast.makeText(getApplicationContext(), "SMS Sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS failed, please try again later!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+
 
     }
 
