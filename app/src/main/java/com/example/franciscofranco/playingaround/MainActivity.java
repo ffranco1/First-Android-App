fd4ceb064cdf9fc9220266275df25ba24466ca7c
Franco <ffranco1@go.pasadena.edu>
2015-10-21 17:03:40 -0700

    request commit

--- app/src/main/java/com/example/franciscofranco/playingaround/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/playingaround/MainActivity.java
@@ -5,18 +5,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button myButton;
     EditText edit_text;
     ImageView image;
+    private static final String QUERY_URL = "http://netflixroulette.net/api/api.php?director=";
 
 
     @Override
@@ -67,13 +78,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        String celebrity = edit_text.getText().toString();
-        edit_text.setText("");
-        // comment 
-
+        queryBooks(edit_text.getText().toString());
     }
 
+    private void queryBooks(String searchString) {
+
+        // Prepare your search string to be put in a URL
+        // It might have reserved characters or something
+        String urlString = "";
+        try {
+            urlString = URLEncoder.encode(searchString, "UTF-8").replace("+", "%20");
+
+            Log.d("LOG", QUERY_URL+urlString);
+
+        } catch (UnsupportedEncodingException e) {
+
+            // if this fails for some reason, let the user know why
+            e.printStackTrace();
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        // Create a client to perform networking
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        // Have the client get a JSONArray of data
+        // and define how to respond
+        Log.d("LOG", "about to do get request");
+        client.get(QUERY_URL + urlString,
+                new JsonHttpResponseHandler() {
 
 
+                    @Override
+                    public void onSuccess(JSONObject jsonObject) {
+                        // Display a "Toast" message
+                        // to announce your success
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+
+                        // 8. For now, just log results
+                        Log.d("LOG", jsonObject.toString());
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                        // Display a "Toast" message
+                        // to announce the failure
+                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
+
+                        // Log error message
+                        // to help solve any problems
+                        Log.e("LOG", statusCode + " " + throwable.getMessage());
+                    }
+                });
+        Log.d("LOG", "finished request");
+    }
 
 }
